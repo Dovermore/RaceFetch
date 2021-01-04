e8fd616e490a1d614abac04a41bfc4d7cf5e7222
Dovermore <calvinyeye0627@gmail.com>
2021-01-04 18:01:05 +1100

    update api and flask, not tested yet

--- app.py
+++ app.py
@@ -1,62 +1,37 @@
 # scaffolding code from https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
-import flask
-from flask import request, jsonify
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine, sessionmaker
+from dbs import Race
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
+api = Api(app)
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+engine = create_engine('sqlite:///foo.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+@api.resource("/")
+class Inex(Resource):
+    def get(self):
+        return "<h1>This is the index page.</h1>"
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
+@api.resource("/race/all")
+class AllRace(Resource):
+    def get(self):
+        results = []
+        for race in session.query(Race).order_by(Race.start):
+            results.append({race.id: {
+                "type": race.rtype,
+                "number": race.rnumber,
+                "name": race.rname,
+                "start": race.rstart
+            }})
+        return jsonify(results)
 
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
 
 app.run()
